01f0862d3fe3fa05e730c7c926625d7b1097de42
Brent Westbrook <brentrwestbrook@gmail.com>
2023-09-20 12:48:27 -0400

    generate table in index and link to svgs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
 //! web ui for debugging torsion multiplicity issues
 
-use std::{collections::HashMap, fmt::Display, fs::File, path::Path};
+use actix_files::NamedFile;
+use std::path::PathBuf;
+use std::{
+    collections::HashMap,
+    error::Error,
+    fmt::Display,
+    fs::{self, File},
+    path::Path,
+    sync::RwLock,
+};
 
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
 use ligand::molecule::Molecule;
 use openff_toolkit::qcsubmit::results::{Entry, ResultCollection};
 
@@ -45,7 +55,79 @@ fn load_records(filename: impl AsRef<Path>) -> Vec<Record> {
         .collect()
 }
 
-fn main() {
+fn load(path: impl AsRef<Path>) -> String {
+    fs::read_to_string(path).unwrap()
+}
+
+type Datum = web::Data<State>;
+
+async fn index(data: Datum) -> impl Responder {
+    use std::fmt::Write;
+    let mut body = String::new();
+    let records = data.records.read().unwrap();
+    writeln!(body, "<table>").unwrap();
+    for record in records.iter() {
+        writeln!(body, "<tr>").unwrap();
+        writeln!(
+            body,
+            "<td><a href=/pic?id={0}>{}</a></td><td>{}</td>",
+            record.id, record.dde
+        )
+        .unwrap();
+        writeln!(body, "</tr>").unwrap();
+        // println!("{} => {}", record.id, map[&record.id]);
+    }
+    writeln!(body, "</table>").unwrap();
+
+    let index = load("index.html");
+    HttpResponse::Ok().body(index.replace("{{body}}", &body))
+}
+
+async fn pic(data: Datum, req: HttpRequest) -> impl Responder {
+    // let records = data.records.read().unwrap();
+    let map = data.map.read().unwrap();
+    let query = req.query_string();
+    let sp: Vec<_> = query.split('=').collect();
+    // TODO cache the generated SVG files
+    let mol = Molecule::from_mapped_smiles(&map[sp[1]]).unwrap();
+    HttpResponse::Ok().body(mol.to_svg())
+}
+
+#[allow(unused)]
+struct State {
+    records: RwLock<Vec<Record>>,
+    map: RwLock<HashMap<String, String>>,
+}
+
+impl State {
+    fn new(records: Vec<Record>, map: HashMap<String, String>) -> Self {
+        Self {
+            records: RwLock::new(records),
+            map: RwLock::new(map),
+        }
+    }
+}
+
+// copy pasta from berry-patch
+macro_rules! file_handlers {
+    ($($name:ident => $path:expr$(,)*)*) => {
+	$(
+	    async fn $name(req: HttpRequest) -> actix_web::Result<NamedFile> {
+		let dir = Path::new($path);
+		let path: PathBuf = req.match_info()
+		    .query("filename").parse().unwrap();
+		Ok(NamedFile::open(dir.join(path))?)
+	    }
+	)*
+    }
+}
+
+file_handlers! {
+    css_file => "css/"
+}
+
+#[actix_web::main]
+async fn main() -> Result<(), Box<dyn Error>> {
     let records = load_records(
         "/home/brent/omsf/projects/benchmarking/output/industry/tm/dde.csv",
     );
@@ -54,9 +136,18 @@ fn main() {
         "/home/brent/omsf/projects/benchmarking/datasets/industry.json",
     );
 
-    for record in &records[..5] {
-        let mol = Molecule::from_mapped_smiles(&map[&record.id]).unwrap();
-        println!("{} => {}", record.id, map[&record.id]);
-        dbg!(mol.to_svg());
-    }
+    let state = web::Data::new(State::new(records, map));
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(state.clone())
+            .route("/", web::get().to(index))
+            .route("/pic", web::get().to(pic))
+            .route("/css/{filename:.*}", web::get().to(css_file))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await?;
+
+    Ok(())
 }
