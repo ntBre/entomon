b19d7a96ef497078f56b59ea381ed33af8bb63fe
Brent Westbrook <brentrwestbrook@gmail.com>
2023-09-21 11:51:27 -0400

    add Math.abs shorthand and compute expr once

--- js/query.js
+++ js/query.js
@@ -11,6 +11,9 @@ function doQuery() {
 	}
 
 	const regex = /\$(\d)/g;
+	const pipe = /\|([^|]+)\|/g;
+	let expr = query.replace(regex, "vals[$1]");
+	expr = expr.replace(pipe, "Math.abs($1)");
 	for (let i = 1; i < rows.length; i++) {
 		let cols = rows[i].getElementsByTagName("td");
 		let vals = new Array();
@@ -19,7 +22,6 @@ function doQuery() {
 			// easily
 			vals[j] = Number(cols[j].innerHTML);
 		}
-		let expr = query.replace(regex, "vals[$1]");
 		if (!eval(expr)) {
 			rows[i].style.display = "none";
 		}
