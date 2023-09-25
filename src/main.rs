8d95c56184504626d33bc0fa2cfe7adb411ed24e
Brent Westbrook <brentrwestbrook@gmail.com>
2023-09-25 10:27:51 -0400

    read config from file, print url

--- src/main.rs
+++ src/main.rs
@@ -3,31 +3,46 @@
 //! web ui for debugging torsion multiplicity issues
 
 use data::{build_rows, load_dataset, load_records};
+use serde::Deserialize;
 use server::{api, css_file, get_data, index, js_file, set_query, State};
-use std::error::Error;
+use std::{error::Error, path::Path};
 
 use actix_web::{web, App, HttpServer};
 
 mod data;
 mod server;
 
+#[derive(Deserialize)]
+struct Config {
+    records: Vec<String>,
+    names: Vec<String>,
+    dataset: String,
+}
+
+impl Config {
+    fn load(path: impl AsRef<Path>) -> Self {
+        let s = std::fs::read_to_string(path).unwrap();
+        toml::from_str(&s).unwrap()
+    }
+}
+
 #[actix_web::main]
 async fn main() -> Result<(), Box<dyn Error>> {
-    let records = [
-        "/home/brent/omsf/projects/benchmarking/output/industry/tm/dde.csv",
-        "/home/brent/omsf/projects/benchmarking/output/industry/sage-tm/dde.csv",
-        "/home/brent/omsf/projects/benchmarking/output/industry/sage-2.1.0/dde.csv",
-    ].into_iter().map(load_records).collect();
+    let Config {
+        records,
+        names,
+        dataset,
+    } = Config::load("testfiles/test.toml");
 
-    let names = vec!["TM".to_owned(), "Sage-TM".to_owned(), "Sage".to_owned()];
+    let records = records.into_iter().map(load_records).collect();
     let rows = build_rows(records);
-
-    let map = load_dataset(
-        "/home/brent/omsf/projects/benchmarking/datasets/industry.json",
-    );
+    let map = load_dataset(dataset);
 
     let state = web::Data::new(State::new(rows, map, names));
 
+    const ADDR: &str = "127.0.0.1";
+    const PORT: u16 = 8080;
+    println!("serving on {ADDR}:{PORT}");
     HttpServer::new(move || {
         App::new()
             .app_data(state.clone())
@@ -38,7 +53,7 @@ async fn main() -> Result<(), Box<dyn Error>> {
             .route("/css/{filename:.*}", web::get().to(css_file))
             .route("/js/{filename:.*}", web::get().to(js_file))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind((ADDR, PORT))?
     .run()
     .await?;
 
